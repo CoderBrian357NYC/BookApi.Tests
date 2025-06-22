a0bff8673d260602400ef863f7f7ca9a1f3f3409
brianT <briansmacmini@BriansMacMinis-Mac-mini.local>
2025-06-22 20:38:37 +0100

    Initial commit for BookApi.Tests project

---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace BookApi.Tests
+{
+    public class BooksControllerTests : IClassFixture<WebApplicationFactory<BookApi.Program>>
+    {
+        private readonly WebApplicationFactory<BookApi.Program> _factory;
+
+        public BooksControllerTests(WebApplicationFactory<BookApi.Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetBooks_ReturnsSuccessAndJsonContent()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/books");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType?.ToString());
+        }
+    }
+}
